fa546be638ff5ced4f11c0a8afdea60366cc2b80
Kinga <knapik.kinga@gmail.com>
2022-02-25 13:57:33 +0100

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PoweredByFlotiq } from 'flotiq-components-react';
 
-const Footer = ({ additionalClass }) => (
+const Footer = ({ additionalClass = [] }) => (
     <footer className={['flex justify-center w-full', ...additionalClass].join(' ')}>
         <div className="font-inter w-auto px-2 md:px-5 py-5 mt-7 md:mt-15
                 inline-flex justify-between md:justify-center items-center
